130a537f149bb0ebfc0cb8ae355ef5380a45068e
Szymon Kubica <szymo.kubica@gmail.com>
2023-12-20 22:11:08 +0100

    Added the example checksum calculating code.

--- fletcher32.bpf.c
+++ fletcher32.bpf.c
@@ -4,26 +4,33 @@
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 
+/*
+ * Discussion points: eBPF seems to be limited w.r.t storing strings on the stack
+ * When I tried including the 360B long string in the function code directly as
+ * a constant. There was an error with illegal memory accesses. It could be
+ * because that string couldn't fit in the stack.
+ *
+ */
+
 SEC("fletcher32")
 int fletcher_32(void *ctx)
 {
     // Similarly to femtocontainers. The checksum algorithm is run on a 360B
     // string (i.e. it contains 360 characters.
+    //
     char *message =
-        "This is a test message for the Fletcher32 checksum algorithm.\n"
-        "It is 360 characters long to replicate the workflow under which the"
-        "femtocontainers implementation was tested.\n"
-        "The aim of this test the performance of the rBPF VM running on a "
-        "microcontroller"
-        "and compare it against the native execution.\n"
-        "The next step: test the same workflow on stm32 microcontroller";
+        "This is a test message for the Fletcher32 checksum algorithm.\n";
 
     uint16_t *data = (uint16_t *)message;
 
-    size_t len = strlen(message);
+    bpf_trace_printk("", 20, *data);
 
-    uint32_t c0, c1;
-    len = (len + 1) & ~1; /* Round up len to words */
+    // Algorithm needs the length in words
+    size_t len = strlen(message) / 2;
+    bpf_trace_printk("", 20, *data);
+
+    uint32_t c0 = 0;
+    uint32_t c1 = 0;
 
     /* We similarly solve for n > 0 and n * (n+1) / 2 * (2^16-1) < (2^32-1)
      * here.
@@ -36,10 +43,12 @@ int fletcher_32(void *ctx)
             blocklen = 360 * 2;
         }
         len -= blocklen;
-        do {
-            c0 = c0 + *data++;
+        for (size_t i = 0; i < blocklen; i += 2) {
+            bpf_trace_printk("", 20, *data);
+            char c = *(data);
+            c0 = c0 + c;
             c1 = c1 + c0;
-        } while ((blocklen -= 2));
+        }
         c0 = c0 % 65535;
         c1 = c1 % 65535;
     }
@@ -47,9 +56,4 @@ int fletcher_32(void *ctx)
     uint32_t checksum = (c1 << 16 | c0);
 
     return checksum;
-}
-
-int main() {
-    int checksum = fletcher_32(NULL);
-    printf("Checksum: %d\n", checksum);
 }
